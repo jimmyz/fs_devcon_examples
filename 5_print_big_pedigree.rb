35634e1720d5504b596bc7273a8579c5c16695ae
Jimmy Zimmerman <jimmy.zimmerman@gmail.com>
2011-02-10 04:45:15 -0700

    Final example updates for RootsTech demos.

--- 5_print_big_pedigree.rb
+++ 5_print_big_pedigree.rb
@@ -2,11 +2,13 @@ require 'rubygems'
 require 'ruby-fs-stack'
 require 'authenticate'
 require 'logger'
-  
+
+$visited_ids = []  
 def print_pedigree(person,level = 0)
   puts ' '*level + person.full_name + " (#{person.id})"
-  print_pedigree person.father, level+1 if person.father && person.father.id != person.id
-  print_pedigree person.mother, level+1 if person.mother && person.mother.id != person.id
+  $visited_ids << person.id
+  print_pedigree person.father, level+1 if person.father && !$visited_ids.include?(person.father.id)
+  print_pedigree person.mother, level+1 if person.mother && !$visited_ids.include?(person.mother.id)
 end
 
 pedigree = Marshal.load(File.read('pedigree_marshalled.txt'))
