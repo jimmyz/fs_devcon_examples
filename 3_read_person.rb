79abb0886d0955f3ddcb11206dda52cc1bd90361
Jimmy Zimmerman <jimmy.zimmerman@gmail.com>
2012-07-12 16:19:04 -0600

    Added a gemfile and a $:.unshift so that the authenticate.rb is required from the local path.

--- 3_read_person.rb
+++ 3_read_person.rb
@@ -1,3 +1,4 @@
+$:.unshift File.dirname(__FILE__)
 require 'rubygems'
 require 'ruby-fs-stack'
 require 'authenticate'
---
+++ Gemfile
@@ -0,0 +1,3 @@
+source 'https://rubygems.org'
+gem "json", "~> 1.7.3"
+gem "ruby-fs-stack", "~> 0.5.3"
